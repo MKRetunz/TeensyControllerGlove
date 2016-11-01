b90d73d2e4d8c0dfe6da55b5c5a63579d5c2f3ec
MKRetunz <Martijnklok@hotmail.nl>
2016-11-01 11:30:57 +0100

    Removed comments

--- GloveController/GloveController.ino
+++ GloveController/GloveController.ino
@@ -9,90 +9,17 @@
  * Note: since we use the serial connection to Unity, most serial.print function calls in the loop 
  * from the original example must be commented out!!
 */
-// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
-// for both classes must be in the include path of your project
 #include "I2Cdev.h"
 
 #include "MPU6050_6Axis_MotionApps20.h"
-//#include "MPU6050.h" // not necessary if using MotionApps include file
-// (Gidi:) in earlier version I replaced this: 
-//#include "MPU6050_6Axis_MotionApps20.h"
-// by
-//#include "MPU6050_9Axis_MotionApps41.h"  // (but I don't see any difference, except with the latter the frequency of the blinking led is lower?)
-//#include "MPU6050.h" // not necessary if using MotionApps include file
 
-// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
-// is used in I2Cdev.h
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
     #include "Wire.h"
 #endif
 
-// class default I2C address is 0x68
-// specific I2C addresses may be passed as a parameter here
-// AD0 low = 0x68 (default for SparkFun breakout and InvenSense evaluation board)
-// AD0 high = 0x69
 MPU6050 mpu;
-//MPU6050 mpu(0x69); // <-- use for AD0 high
 
-/* =========================================================================
-   NOTE: In addition to connection 3.3v, GND, SDA, and SCL, this sketch
-   depends on the MPU-6050's INT pin being connected to the Arduino's
-   external interrupt #0 pin. On the Arduino Uno and Mega 2560, this is
-   digital I/O pin 2.
-   
-   Remark Gidi: on the Teensy2.0 and Teensy2.0++ interrupt #0 is the same pin as
-   one of SDA or SCL - and apparently it works without connecting the INT pin 
-   on the MPU-9150 breakout board??
-   (I even tested it on the UNO: also works without this pin connected?)
-   Later I realized that i can set another interrupt, e.g. interupt 2 on the Teensy2.0
-   (= pin 7 on the Teensy2.0). I tried it, but saw no change in behavior in Unity
- * ========================================================================= */
-
-/* =========================================================================
-   NOTE: Arduino v1.0.1 with the Leonardo board generates a compile error
-   when using Serial.write(buf, len). The Teapot output uses this method.
-   The solution requires a modification to the Arduino USBAPI.h file, which
-   is fortunately simple, but annoying. This will be fixed in the next IDE
-   release. For more info, see these links:
-
-   http://arduino.cc/forum/index.php/topic,109987.0.html
-   http://code.google.com/p/arduino/issues/detail?id=958
- * ========================================================================= */
-
-// uncomment "OUTPUT_READABLE_QUATERNION" if you want to see the actual
-// quaternion components in a [w, x, y, z] format (not best for parsing
-// on a remote host such as Processing or something though)
 #define OUTPUT_READABLE_QUATERNION
-
-// uncomment "OUTPUT_READABLE_EULER" if you want to see Euler angles
-// (in degrees) calculated from the quaternions coming from the FIFO.
-// Note that Euler angles suffer from gimbal lock (for more info, see
-// http://en.wikipedia.org/wiki/Gimbal_lock)
-//#define OUTPUT_READABLE_EULER
-
-// uncomment "OUTPUT_READABLE_YAWPITCHROLL" if you want to see the yaw/
-// pitch/roll angles (in degrees) calculated from the quaternions coming
-// from the FIFO. Note this also requires gravity vector calculations.
-// Also note that yaw/pitch/roll angles suffer from gimbal lock (for
-// more info, see: http://en.wikipedia.org/wiki/Gimbal_lock)
-//#define OUTPUT_READABLE_YAWPITCHROLL
-
-// uncomment "OUTPUT_READABLE_REALACCEL" if you want to see acceleration
-// components with gravity removed. This acceleration reference frame is
-// not compensated for orientation, so +X is always +X according to the
-// sensor, just without the effects of gravity. If you want acceleration
-// compensated for orientation, us OUTPUT_READABLE_WORLDACCEL instead.
-//#define OUTPUT_READABLE_REALACCEL
-
-// uncomment "OUTPUT_READABLE_WORLDACCEL" if you want to see acceleration
-// components with gravity removed and adjusted for the world frame of
-// reference (yaw is relative to initial orientation, since no magnetometer
-// is present in this case). Could be quite handy in some cases.
-//#define OUTPUT_READABLE_WORLDACCEL
-
-// uncomment "OUTPUT_TEAPOT" if you want output that matches the
-// format used for the InvenSense teapot demo
-//#define OUTPUT_TEAPOT
 
 #define LED_PIN 13 // (Arduino is 13, Teensy2.0 is 11, Teensy2.0++ is 6)
 bool blinkState = false;
@@ -149,8 +76,7 @@ void setup() {
     pinMode(INTERRUPT_PIN, INPUT); // Gidi: sets the digital pin as input,  necessary for the interrupt
     pinMode(LMOUSE_PIN, INPUT); 
     pinMode(ESC_PIN, INPUT);
-
-    // join I2C bus (I2Cdev library doesn't do this automatically)
+    
     #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
         Wire.begin();
         TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
@@ -158,20 +84,8 @@ void setup() {
         Fastwire::setup(400, true);
     #endif
     
-    // initialize serial communication
-    // (115200 chosen because it is required for Teapot Demo output, but it's
-    // really up to you depending on your project)
     Serial.begin(9600);
-    // Gidi: commented out the next while loop because it gave errors and I am not using Arduino Leonardo
-    while (!Serial); // wait for Leonardo enumeration, others continue immediately
-
-    // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3v or Ardunio
-    // Pro Mini running at 3.3v, cannot handle this baud rate reliably due to
-    // the baud timing being too misaligned with processor ticks. You must use
-    // 38400 or slower in these cases, or use some kind of external separate
-    // crystal solution for the UART timer.
-    
-    // initialize device
+    while (!Serial); 
     Serial.println(F("Initializing I2C devices..."));
     mpu.initialize();
 
@@ -180,33 +94,13 @@ void setup() {
     Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
     // wait for ready
-    delay(5000);  // (Gidi) I added this, just to be sure 
-    //Serial.println(F("\nSend any character to begin DMP programming and demo: "));
-    //while (Serial.available() && Serial.read()); // empty buffer
-    //while (!Serial.available());                 // wait for data
-    //while (Serial.available() && Serial.read()); // empty buffer again
-
-    // load and configure the DMP
+    delay(50);
     Serial.println(F("Initializing DMP..."));
     devStatus = mpu.dmpInitialize();
-
-    // turn on the DMP, now that it's ready
-    //Serial.println(F("Enabling DMP..."));
     mpu.setDMPEnabled(true);
-
-    // enable Arduino interrupt detection
-    //Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
-    attachInterrupt(INTERRUPT_PIN, dmpDataReady, RISING);    // (Gidi) if you use the interrupts, you can set this to interrupt 2 for the Teensy2.0 
     mpuIntStatus = mpu.getIntStatus();
-
-    // set our DMP Ready flag so the main loop() function knows it's okay to use it
-    //Serial.println(F("DMP ready! Waiting for first interrupt..."));
-        dmpReady = true;
-
-    // get expected DMP packet size for later comparison
+    dmpReady = true;
     packetSize = mpu.dmpGetFIFOPacketSize();
-
-    // configure LED for output
     pinMode(LED_PIN, OUTPUT);
 }
 
@@ -215,49 +109,22 @@ void setup() {
 // ================================================================
 
 void loop() {
-    // if programming failed, don't try to do anything
-    if (!dmpReady) return;
-
-    // wait for MPU interrupt or extra packet(s) available
-    while (!mpuInterrupt && fifoCount < packetSize) {
-        // other program behavior stuff here
-        // if you are really paranoid you can frequently test in between other
-        // stuff to see if mpuInterrupt is true, and if so, "break;" from the
-        // while() loop to immediately process the MPU data
-        // .
-        // .
-        // .
-        // Gidi: N.B. I noticed that the program never actually gets into this
-        // while loop (because I did not connect the interrupt pin ;-)), 
-        // so I placed my code outside of this loop.
-        // Later I tested it on the Uno and on the Teensy2.0 with the interrupt pin
-        // attached, and I was able to put my communication code inside this loop
-        
-        // +++++++++++++++++++++++++++++++++++
-    // Gidi: with the interrupt pin attached, 
-    // I placed my code to communicate with Unity inside the loop
-    //
+    if (!dmpReady) 
+      while (!mpuInterrupt && fifoCount < packetSize) {
     if(Serial.available()) {
       char inByte = Serial.read();
 
       if (inByte == GIVE_INPUT) {
-        // pass input values to computer
         passValuesToComputer();
       }
       else if (inByte == ISSUE_COMMAND) {
-        // parse commands from computer
         parseCommands();
-      }  // else, ignore this byte
-    }
-    // +++++++++++++++++++++++++++++++++++
-
-        
+      } 
+    }    
     }
 
      LmouseState = digitalRead(LMOUSE_PIN);
      EscState = digitalRead(ESC_PIN);
-    // check if the pushbutton is pressed.
-    // if it is, the buttonState is LOW:
     if (LmouseState == LOW) {      
       //Mouse.click();
       //Mouse.set_buttons(1, 0, 0);
@@ -266,37 +133,18 @@ void loop() {
       //Keyboard.set_key1(KEY_ESC);
       //Keyboard.send_now();
     }
-
-    // reset interrupt flag and get INT_STATUS byte
     mpuInterrupt = false;
     mpuIntStatus = mpu.getIntStatus();
-
-    // get current FIFO count
     fifoCount = mpu.getFIFOCount();
-
-    // check for overflow (this should never happen unless our code is too inefficient)
     if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
-        // reset so we can continue cleanly
         mpu.resetFIFO();
-        //Serial.println(F("FIFO overflow!"));
-
-    // otherwise, check for DMP data ready interrupt (this should happen frequently)
     } else if (mpuIntStatus & 0x02) {
-        // wait for correct available data length, should be a VERY short wait
         while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-
-        // read a packet from FIFO
         mpu.getFIFOBytes(fifoBuffer, packetSize);
-        
-        // track FIFO count here in case there is > 1 packet available
-        // (this lets us immediately read more without waiting for an interrupt)
         fifoCount -= packetSize;
-
-        // display quaternion values in easy matrix form: w x y z
-        mpu.dmpGetQuaternion(&q, fifoBuffer); // Gidi: we just read the quaternion q, and process later
+        mpu.dmpGetQuaternion(&q, fifoBuffer);
         Serial.print("quat\t");
-        Serial.print(q.w);
-            
+        Serial.print(q.w);         
         Serial.print("\t");
         Serial.print(q.x);
         Serial.print("\t");
@@ -311,34 +159,21 @@ void loop() {
 
 void passValuesToComputer()
 {
-    // pass the Quaternion values as fake analog signals, and scale them to integers 0..1000
-  
-    // construct string from read values
     String sOutput = "";
-
-    // add value analog A0  
     int adc0 = (int) (q.x * 1000);
     sOutput = sOutput + adc0;
-
-    // add value analog A1  
     int adc1 = (int) (q.y * 1000);
     sOutput = sOutput + "," + adc1;
-
-    // add value analog A2  
     int adc2 = (int) (q.z * 1000);
     sOutput = sOutput + "," + adc2;
-
-    // add value analog A3  
     int adc3 = (int) (q.w * 1000);
     sOutput = sOutput + "," + adc3;
 
     Serial.println(sOutput);
 }
 
-// parse commands received from the computer
 void parseCommands() {
-  // read next byte as command
-  delay(1);  // a small delay seems necessary to get Serial.available working again?
+  delay(1);
   if(Serial.available()) {
         char inByte = Serial.read();
         if (inByte == SET_LED_ON) {
